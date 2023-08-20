8998cb8e33e97274a867672ee98b555e0738bb3b
youssefali715 <youssef.sls.2020@gmail.com>
2023-08-20 16:57:55 +0300

    Added Week 5 Challenges

---
+++ Week 5/if_challenge.js
@@ -0,0 +1,45 @@
+/*
+  If Condition Challenge
+*/
+
+let a = 10;
+
+if (a < 10) {
+  console.log(10);
+} else if (a >= 10 && a <= 40) {
+  console.log("10 To 40");
+} else if (a > 40) {
+  console.log("> 40");
+} else {
+  console.log("Unknown");
+}
+
+a < 10
+  ? console.log(10)
+  : a >= 10 && a <= 40
+  ? console.log("10 To 40")
+  : a > 40
+  ? console.log("> 40")
+  : console.log("Unknown");
+
+let st = "Elzero Web School";
+
+if (String(st.length * 2) === "34") {
+  console.log("Good");
+}
+// W Position May Change
+if (st.charAt(st.indexOf("W")).toLowerCase() === "w") {
+  console.log("Good");
+}
+
+if (st !== "string") {
+  console.log("Good");
+}
+
+if (typeof st.length === "number") {
+  console.log("Good");
+}
+
+if (st.slice(0, 6).repeat(2) === "ElzeroElzero") {
+  console.log("Good");
+}
---
+++ Week 5/swaitch_if_challenge.js
@@ -0,0 +1,45 @@
+/*
+  Switch Challenge
+*/
+let job = "Manager";
+let salary = 0;
+
+switch (job) {
+  case "Manager":
+    salary = 8000;
+    break;
+  case "IT":
+  case "Support":
+    salary = 6000;
+    break;
+  case "Developer":
+  case "Designer":
+    salary = 7000;
+    break;
+  default:
+    salary = 4000;
+    break;
+}
+console.log(salary);
+
+/*
+  If Challenge
+*/
+
+let holidays = 0;
+let money = 0;
+
+if (holidays == 0) {
+  money = 5000;
+} else if (holidays == 1 || holidays == 2) {
+  money = 3000;
+} else if (holidays == 3) {
+  money = 2000;
+} else if (holidays == 4) {
+  money = 1000;
+} else if (holidays == 5) {
+  money = 0;
+} else {
+  money = 0;
+}
+console.log(`My Money is ${money}`);
